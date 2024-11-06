645716ac3cdac5c6ed8ca4dd75972fcbe387e736
krzysztofpaliga <paligakrzychu@gmail.com>
2024-11-06 11:46:20 +0100

    Adds Routes and Pages; just before switching to json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
